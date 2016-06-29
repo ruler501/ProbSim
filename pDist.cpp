aca98ecf8c9724cfeccb556c95be01e472c0fd95
Devon Richards <d.ruler501@gmail.com>
2016-06-29 01:43:58 -0500

    Small touchup

--- pDist.cpp
+++ pDist.cpp
@@ -1,15 +1,21 @@
-#import <algorithm>
-#import <functional>
-#import <iomanip>
-#import <iostream>
-#import <map>
-#import <numeric>
-#import <sstream>
-#import <string>
-#import <thread>
-#import <type_traits>
-#import <utility>
-#import <vector>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+//#include "Cache.h"
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 /*****************************************************************************
  * BEGIN BOILERPLATE UTILITIES                                               *
@@ -142,9 +148,6 @@ protected:
         }
     }
 
-    ProbabilityDist(const std::map<T, double>& d)
-    : dist(d) {prune();}
-    
     template <typename U>
     friend class recDefD2D;
     
@@ -159,6 +162,19 @@ protected:
     
     friend ProbabilityDist<int> sumProb(int n, int s);
 
+    friend void plotDist(ProbabilityDist<T> d, const char* format=nullptr){
+        std::vector<T> x(d.dist.size());
+        std::vector<double> y(d.dist.size());
+        int i = 0;
+        for (auto& e : d.dist){
+            x[i] = e.first;
+            y[i] = e.second;
+            i++;
+        }
+        if(format != nullptr) plt::plot(x, y, format);
+        else plt::plot(x,y);
+    }
+
     template <typename U>
     friend class ProbabilityDist;
 public:
@@ -167,6 +183,11 @@ public:
 
     ProbabilityDist(T start)
     : dist{{start,1}} {}
+
+    ProbabilityDist(const std::map<T, double>& d)
+    : dist(d) {prune();}
+    
+    std::map<T,double>& getDist(){ return dist; }
 
     template <typename U>
     ProbabilityDist(ProbabilityDist<U> d){
@@ -200,6 +221,45 @@ public:
     }
 };
 
+std::map<int, double> addDist(std::map<int, double> x,
+                              std::map<int, double> y){
+    std::map<int, double> tDist;
+    int minX = x.begin()->first;
+    int maxX = x.rbegin()->first;
+    int minY = y.begin()->first;
+    int maxY = y.rbegin()->first;
+    int maxVal = maxX + maxY;
+    int minVal = minX + minY;
+    for(int i=minVal; i <= maxVal; i++){
+        double tVal = 0;
+        for(int j=minX; j <= maxX; j++){
+            tVal += x[j]*y[i-j];
+        }
+        tDist[i] = tVal;
+    }
+    return tDist;
+}
+
+ProbabilityDist<int> sumProb(int n, int s){
+    std::map<int, double> distX;
+    std::map<int, double> distZ;
+    distZ[0] = 1.f;
+    double baseProb = 1.f/s;
+    for(int i=1; i<=s; i++) { distX[i] = baseProb; }
+    while(n > 1){
+        if(n % 2){
+            distZ = addDist(distX, distZ);
+            distX = addDist(distX, distX);
+        }
+        else{
+            distX = addDist(distX, distX);
+        }
+        n /= 2;
+    }
+    return ProbabilityDist<int>(addDist(distX, distZ));
+}
+
+/*
 ProbabilityDist<int> sumProb(int n, int s){
     if(n <= 0 || s <= 0) return ProbabilityDist<int>(0);
     double iProb = 1.f/s;
@@ -221,6 +281,7 @@ ProbabilityDist<int> sumProb(int n, int s){
     }
     return ProbabilityDist<int>(distN);
 }
+*/
 
 template <typename T>
 struct recDefD2D{
@@ -247,6 +308,7 @@ struct recDefD2D{
 template <typename Callable, typename T, typename... Args>
 auto distToDists(Callable tr, ProbabilityDist<T> p, Args... args) -> typename std::result_of<Callable(T, typename extractT<Args>::type...)>::type{ 
     using target = typename extractT<typename std::result_of<Callable(T, typename extractT<Args>::type...)>::type>::type;
+    using ftype = std::function<ProbabilityDist<target>(T, typename extractT<Args>::type...)>;
     int threads = std::thread::hardware_concurrency();
     std::vector<std::thread*>runThreads;
     std::vector<std::map<target, double>> dists(threads);
@@ -257,7 +319,13 @@ auto distToDists(Callable tr, ProbabilityDist<T> p, Args... args) -> typename st
     }
     for(int i = 0; i < threads; i++){
         if(!sDists[i].empty()){
-            runThreads.push_back(new std::thread(&(recDefD2D<target>::template call<decltype(curry(tr)), T, Args...>), curry(tr), std::ref(dists[i]), std::vector<double>(), ProbabilityDist<T>(sDists[i]), args...));
+            runThreads.push_back(new std::thread(
+                        recDefD2D<target>::template call<decltype(curry(ftype(tr))), T, Args...>,
+                        curry(ftype(tr)),
+                        std::ref(dists[i]),
+                        std::vector<double>(),
+                        ProbabilityDist<T>(sDists[i]),
+                        args...));
         }
     }
     std::map<target,double> dist;
@@ -291,6 +359,7 @@ struct recDefTD{
 template <typename Callable, typename T, typename... Args>
 auto transformDists(Callable tr, ProbabilityDist<T> p, Args... args) -> ProbabilityDist<typename std::result_of<Callable(T, typename extractT<Args>::type...)>::type>{ 
     using target = typename std::result_of<Callable(T, typename extractT<Args>::type...)>::type;
+    using ftype = std::function<target(T, typename extractT<Args>::type...)>;
     int threads = std::thread::hardware_concurrency();
     std::vector<std::thread*>runThreads;
     std::vector<std::map<target, double>> dists(threads);
@@ -301,7 +370,13 @@ auto transformDists(Callable tr, ProbabilityDist<T> p, Args... args) -> Probabil
     }
     for(int i = 0; i < threads; i++){
         if(!sDists[i].empty()){
-            runThreads.push_back(new std::thread(&(recDefTD<target>::template call<decltype(curry(tr)), T, Args...>), curry(tr), std::ref(dists[i]), std::vector<double>(), ProbabilityDist<T>(sDists[i]), args...));
+            runThreads.push_back(new std::thread(
+                        recDefD2D<target>::template call<decltype(curry(ftype(tr))), T, Args...>,
+                        curry(ftype(tr)),
+                        std::ref(dists[i]),
+                        std::vector<double>(),
+                        ProbabilityDist<T>(sDists[i]),
+                        args...));
         }
     }
     std::map<target,double> dist;
@@ -313,12 +388,28 @@ auto transformDists(Callable tr, ProbabilityDist<T> p, Args... args) -> Probabil
     return ProbabilityDist<target>(dist);
 }
 
-double sumAny(int a, float b, double c){
-    return a + b + c;
+double averageDist(ProbabilityDist<int> d){
+    double t = 0;
+    for(auto& e : d.getDist()){
+        t += e.first*e.second;
+    }
+    return t;
 }
 
+#define MAXN 2048
+#define MAXS 32
+std::map<int, double>* rCache[MAXN][MAXS];
 ProbabilityDist<int> rollAgain(int n, int s){
-    return sumProb(n-1,s);
+    if(s < 2 || s > 1 + MAXS || n < 2 || n > MAXN + 1) return sumProb(n - 1, s); 
+    if(rCache[n-2][s-2] == nullptr){
+        for(int i=3; i <= n; i++){
+            if(rCache[n-i][s-2] != nullptr){
+                rCache[n-2][s-2] = new std::map<int,double>(addDist(sumProb(i-2, s).getDist(), *rCache[n-i][s-2]));
+                break;
+            }
+        }
+    }
+    return *rCache[n-2][s-2];
 }
 
 bool heroWon(std::pair<int,int> life){
@@ -334,34 +425,187 @@ std::pair<int,int> newLife(std::pair<int, int> life, int hd, int bd){
         life.second = std::max(0, life.second - hd);
     }
     if(life.second > 0){
-        life.first = std::max(0, life.first - bd);;
+        if(life.first > 0)
+            life.first = std::max(0, life.first - bd + 9);;
     }
     return life;
 }
-    
+
+ProbabilityDist<int> benDamage(int d1, int d2){
+    if (d1 == 1) return ProbabilityDist<int>(0);
+    int n = 0, m = 0;
+    if (d1 >= 19){
+        n += 2;
+        m += 1;
+    }
+    else if (d1 >= 9){
+        n += 1;
+        m += 1;
+    }
+    if (d2 >= 19){
+        n += 2;
+        m += 1;
+    }
+    else if (d2 >= 9){
+        n += 1;
+        m += 1;
+    }
+    ProbabilityDist<int> d = sumProb(n, 8);
+    for(int i=0; i<m; i++) d = transformDists([](int x){ return x+4; }, d);
+    return d;
+}
+
+ProbabilityDist<int> josephDamage(int d1, int d2){
+    if (d1 == 1) return ProbabilityDist<int>(0);
+    int k = 0, n = 0, m = 0;
+    if (d1 == 20){
+        n += 2;
+        k += 6;
+        m += 1;
+    }
+    else if (d1 >= 9){
+        n += 1;
+        k += 3;
+        m += 1;
+    }
+    if (d2 >= 19){
+        n += 2;
+        k += 6;
+        m += 1;
+    }
+    else if (d2 >= 9){
+        n += 1;
+        k += 6;
+        m += 1;
+    }
+    ProbabilityDist<int> d = transformDists([](int x, int y){ return x+y; }, sumProb(n, 8), sumProb(k, 6));
+    for(int i=0; i<m; i++) d = transformDists([](int x){ return x+4; }, d);
+    return d;
+}
+
+ProbabilityDist<int> garrettDamage(int d1, int d2){
+    if (d1 == 1) return ProbabilityDist<int>(0);
+    int n = 0, m = 0;
+    if (d1 == 20){
+        n += 8;
+        m += 1;
+    }
+    else if (d1 >= 13){
+        n += 4;
+        m += 1;
+    }
+    if (d2 == 20){
+        n += 8;
+        m += 1;
+    }
+    else if (d2 >= 13){
+        n += 4;
+        m += 1;
+    }
+    ProbabilityDist<int> d = sumProb(n, 6);
+    for(int i=0; i<m; i++) d = transformDists([](int x){ return x+6; }, d);
+    return d;
+}
+
+ProbabilityDist<int> punchDamage(int d1, int d2){
+    if (d1 == 1) return ProbabilityDist<int>(0);
+    int n = 0;
+    if(d1 == 20) n += 8;
+    else if(d1 >= 9) n += 7;
+    if(d2 == 20) n += 8;
+    else if(d2 >= 9) n += 7;
+    return ProbabilityDist<int>(n);
+}
+
+std::pair<int, int> oneAttack(std::pair<int, int> life, int hd){
+    if (life.first > 0){
+        life.second = std::max(0, life.second - hd);
+    }
+    return life;
+}
+
+int hitReg(int a, int b, int c){
+    int r = 0;
+    if(a >= 5) r++;
+    if(b >= 5) r++;
+    if(c >= 5) r++;
+    return r;
+}
+
+int hitReg2(int a, int b){
+    int r = 0;
+    if(a >= 5) r++;
+    if(b >= 5) r++;
+    return r;
+}
+
+int hitPaladin(int a, int b, int c){
+    int r = 0;
+    if(a >= 5) r++;
+    if(b >= 5) r++;
+    if(c >= 5) r++;
+    if(a == b || b == c || c == a) return 3;
+    return r;
+}
+
+int hitPaladin2(int a, int b){
+    int r = 0;
+    if(a >= 5) r++;
+    if(b >= 5) r++;
+    if(a == b) return 2;
+    return r;
+}
 
 int main(int argc, char* argv[]){
-    //ProbabilityDist<int> d6 = sumProb(1,200);
+    ProbabilityDist<int> d6 = sumProb(1,6);
+    ProbabilityDist<int> d = transformDists(hitReg, d6, d6, d6);
+    ProbabilityDist<int> p = transformDists(hitPaladin, d6, d6, d6);
+    ProbabilityDist<int> d2 = transformDists(hitReg2, d6, d6);
+    ProbabilityDist<int> p2 = transformDists(hitPaladin2, d6, d6);
+    std::cout << d << ' ' << averageDist(d) << '\t' << d2 << ' ' << averageDist(d2) << std::endl << p << ' ' << averageDist(p) << '\t' << p2 << ' ' << averageDist(p2) << std::endl;
+    
+    //plotDist(d);
+    plotDist(d2);
+    //plotDist(p);
+    plotDist(p2);
+    plt::show();
     //ProbabilityDist<float> d6f(d6);
     //ProbabilityDist<double> d6d(d6);
     /* ProbabilityDist<int> d20 = sumProb(1,20); */
     /* //std::cout << d6 << std::endl << d6f << std::endl << d6d << std::endl; */
     //std::cout << ProbabilityDist<int>(transformDists(sumAny, d6, d6f, d6d)) << std::endl << std::endl;
     /* std::cout << sumProb(0,6) << std::endl << std::endl; */
-    //std::cout << distToDists(rollAgain, sumProb(1,1000), sumProb(1,2)) << std::endl;
-    ProbabilityDist<int> hd = sumProb(10,4);
-    ProbabilityDist<int> bd = sumProb(2,20);
-    ProbabilityDist<std::pair<int,int>> life(std::make_pair(100,100));
-    ProbabilityDist<bool> won(false);
-    ProbabilityDist<bool> done(false);
-    while(done.valueAt(false) > 0.0001){
-         life = transformDists(newLife, life, hd, bd);
-         won = transformDists(heroWon, life);
-         done = transformDists(battleDone, life);
-         std::cout << won << '\t' << done << '\t';
-         std::cout.unsetf(std::ios_base::fixed);
-         std::cout << done.valueAt(false) << std::endl;
-    }
+    /* for(int i=0; i < MAXS; i++){ */
+    /*     rCache[0][i] = new std::map<int, double>(sumProb(1,i+2).getDist()); */
+    /*     for(int j=1; j < MAXN; j++){ */
+    /*         rCache[j][i] = nullptr; */
+    /*     } */
+    /* } */
+    /* ProbabilityDist<int> d = distToDists(rollAgain, sumProb(1,1000), ProbabilityDist<int>(20)); */ 
+    //plotDist(d);
+    //plt::show();
+    //    ProbabilityDist<int> hd = distToDists(bossHit, transformDists(max, sumProb(1,20), sumProb(1,20)));
+    //std::cout << sumProb(800,6) << std::endl;
+    /* ProbabilityDist<int> d20 = sumProb(1,20); */
+    /* ProbabilityDist<int> ad20 = transformDists(max, d20, d20); */
+    /* ProbabilityDist<int> hd0 = distToDists(benDamage, d20, d20); */
+    /* ProbabilityDist<int> hd = transformDists(div2, distToDists(benDamage, ad20, ad20)); */
+    /* hd = transformDists(addBoth, hd, transformDists(div2, distToDists(josephDamage, ad20, ad20))); */
+    /* ProbabilityDist<int> bd = distToDists(garrettDamage, ad20, ad20); */
+    /* ProbabilityDist<std::pair<int,int>> life(std::make_pair(59, 71)); */
+    /* life = transformDists(oneAttack, life, hd0); */
+    /* life = transformDists(newLife, life, hd0, bd); */
+    /* ProbabilityDist<int> won = transformDists(heroWon, life); */
+    /* ProbabilityDist<int> done = transformDists(battleDone, life); */
+    /* std::cout << won << '\t' << done << std::endl; */
+    /* while(done.valueAt(false) > 0.00005){ */
+    /*      life = transformDists(newLife, life, hd, bd); */
+    /*      won = transformDists(heroWon, life); */
+    /*      done = transformDists(battleDone, life); */
+    /*      std::cout << won << '\t' << done << std::endl; */
+   /* //      plotDist(transformDists(extractSecond, life)); */
+    /* } */
+   /* // plt::show(); */
 }
 
 
